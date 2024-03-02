c110d0a38f76a62553b8a7ac89fe28d1f64d2151
gabbygood <85273191+gabbygood@users.noreply.github.com>
2024-03-02 20:10:04 +0800

    deleted main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-const password = prompt("What is the password of the site")
-const mainBodyEl = document.querySelector(".main-body")
-if (password === "Gabrielle") {
-    mainBodyEl.style.visibility = 'visible'
-} else {
-    mainBodyEl.style.visibility = 'hidden'
-}
